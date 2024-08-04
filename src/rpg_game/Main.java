d76b2a5cdd1ccd915e107bcb0862a4d467d9cd91
João Dias <joao.dias@orpen.com.br>
2024-08-04 01:31:27 -0300

    feat: testa sistema de ataque dos personagens

--- src/rpg_game/Main.java
+++ src/rpg_game/Main.java
@@ -1,11 +1,12 @@
 package rpg_game;
-
 public class Main {
-
 	public static void main(String[] args) {
-		Dog myDog = new Dog("Bento", "Shitzu", 5);
-		
-		myDog.bark();
-	}
+		EnemyFactory enemyFactory = new EnemyFactory();
 
+		Player player = new Player("Dolphin Love");
+		Enemy orc = enemyFactory.createEnemy("Orc");
+
+		player.attack(orc);
+		orc.attack(player);		
+	}
 }
