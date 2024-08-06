2cf4136e0c225240ed611446b4ada2a843bba0b4
João Dias <joao.dias@orpen.com.br>
2024-08-06 16:08:24 -0300

    feat: cria protótipo da função responsável por gerenciar a batalha desenvolve lógica do turno do jogador

--- src/BattleSystem.java
+++ src/BattleSystem.java
@@ -1,12 +1,43 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class BattleSystem {
-  public void startBattle(Player player, HashMap<String, Enemy> enemies) {}
+	Scanner scanner = new Scanner(System.in);
 
-	public void printAllBattleEnemies(HashMap<String, Enemy> enemies) {
+  public void startBattle(Player player, HashMap<String, Enemy> enemies) {
+		boolean isBattleOngoing = true;
+
+		System.out.println(player.getName() + " entrou em uma batalha!");
+		while (isBattleOngoing) {
+			// Turno do jogador
+			System.out.println("Sua vez! Digite o nome do inimigo que queira atacar:");
+			this.printAllBattleEnemies(enemies);
+
+			boolean enemyExistsInBattle;
+			String enemyChosen;
+			do {	
+				enemyChosen = scanner.nextLine().toUpperCase();
+				enemyExistsInBattle = enemies.containsKey(enemyChosen);
+
+				if (!enemyExistsInBattle) {
+					System.out.println("Este inimigo não está presente na batalha, escolha outro!");
+				}
+			} while (!enemyExistsInBattle);
+
+			Enemy enemyToAttack = enemies.get(enemyChosen);
+			System.out.println("> " + player.getName() + " decidiu atacar " + enemyToAttack.getName());
+
+			player.attack(enemyToAttack);
+			
+			isBattleOngoing = false;
+		}
+
+	}
+
+	private void printAllBattleEnemies(HashMap<String, Enemy> enemies) {
 		for (Map.Entry<String, Enemy> enemy : enemies.entrySet()){
-			System.out.println("> " + enemy.getKey() + " (" + enemy.getValue().getHealthPoints() + "HP)");
+			System.out.println("> " + enemy.getValue().getName() + " (" + enemy.getValue().getHealthPoints() + "HP)");
 		}
 	}
 }
