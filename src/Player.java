a1c7ea2bfb710f175d74423754e7628ae91e1afe
João Dias <joao.dias@orpen.com.br>
2024-08-06 21:03:35 -0300

    feat: cria método para o jogador usar itens de cura

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,13 @@
+import inventory.healing.*;
+
 public class Player extends Character {
 	public Player(String name) {
 		super(name, 5, 30);
+	}
+
+	public void useHealingItem(HealingItem item) {
+		System.out.println("> " + this.getName() + " usou " + item.getName() + "!");
+
+		this.recoverLife(item.getHealingAmount());
 	}
 }
