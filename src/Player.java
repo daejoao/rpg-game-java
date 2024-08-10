9e022286d4471aae929562e4cc5f6083e04931a2
João Dias <joao.dias@orpen.com.br>
2024-08-09 22:11:45 -0300

    feat: adiciona função para o jogador usar itens de cura

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
 import inventory.healing.*;
-
+import inventory.Inventory;
 public class Player extends Character {
+	Scanner scanner = new Scanner(System.in);
+	public Inventory inventory = new Inventory();
+
 	public Player(String name) {
 		super(name, 5, 30);
 	}
 
-	public void useHealingItem(HealingItem item) {
-		System.out.println("> " + this.getName() + " usou " + item.getName() + "!");
+	public void useHealingItem() {
+		HealingItem healingItem;
+		
+		do {
+			this.inventory.seeHealingItens();
 
-		this.recoverLife(item.getHealingAmount());
+			System.out.println("Digite o ID de um item para usá-lo: ");
+			Integer itemId = scanner.nextInt();
+
+			healingItem = this.inventory.getHealingItem(itemId);
+
+			if (healingItem == null) {
+				System.out.println("Escolha outro: ");
+			}
+		} while (healingItem == null);
+
+		this.recoverLife(healingItem.getHealingAmount());
 	}
 }
